abe37006d1ca16915278970f500b5eda476e0ed0
Gustavo Alcantara <gustavo.v.alcantara@gmail.com>
2024-08-16 12:36:08 -0300

    feat:write intrinsic parameters to config file

--- src/stereo/stereo.cpp
+++ src/stereo/stereo.cpp
@@ -40,11 +40,12 @@ int main(int argc, char **argv)
 
     if(left_camera_info_received && right_camera_info_received){
         RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Camera info received");
-        write_config_file(left_camera_info, right_camera_info);
     }
 
     else
         RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "No camera info provided");
+
+    write_config_file(left_camera_info, right_camera_info);
 
     const string path_to_vocabulary = "/ws/src/ros2_orbslam3/vocabulary/ORBvoc.txt";
     const string path_to_settings = "/ws/src/ros2_orbslam3/config/stereo/config.yaml";
@@ -61,29 +62,32 @@ int main(int argc, char **argv)
 }
 
 void write_config_file(sensor_msgs::msg::CameraInfo left_camera_info, sensor_msgs::msg::CameraInfo right_camera_info){
-    //TODO
-    /*ofstream MyFile("/ws/src/ros2_orbslam3/config/stereo/config.yaml");
+    
+    ofstream MyFile("/ws/src/ros2_orbslam3/config/stereo/config.yaml");
 
-    MyFile << '%YAML:1.0' << endl;
-    MyFile << 'File.version: "1.0"' << endl;
+    MyFile << "%YAML:1.0" << endl;
+    MyFile << "File.version: '1.0'" << endl;
 
-    MyFile << 'Camera.type: "'<< cam_info.distortion_model <<'"'<< endl;
+    MyFile << "Camera.type: '"<< left_camera_info.distortion_model <<"'"<< endl;
 
-    MyFile << 'Camera1.fx: "'<< cam_info.distortion_model <<'"'<< endl;
-    MyFile << 'Camera1.fy: "'<< cam_info.distortion_model <<'"'<< endl;
+    MyFile << "Camera1.fx: " << left_camera_info.k[0] << endl;
+    MyFile << "Camera1.fy: "<< left_camera_info.k[4] << endl;
 
-    MyFile << 'Camera1.cx: "'<< cam_info.distortion_model <<'"'<< endl;
-    MyFile << 'Camera1.cy:"'<< cam_info.distortion_model <<'"'<< endl;
+    MyFile << "Camera1.cx: "<< left_camera_info.k[2] << endl;
+    MyFile << "Camera1.cy: "<< left_camera_info.k[5]  << endl;
 
-    MyFile << 'Camera1.k1: "'<< cam_info.distortion_model <<'"'<< endl;
-    MyFile << 'Camera1.k2: "'<< cam_info.distortion_model <<'"'<< endl;
-    MyFile << 'Camera1.k3: "'<< cam_info.distortion_model <<'"'<< endl;
+    /*MyFile << 'Camera1.k1: "'<< left_camera_info.distortion_model <<'"'<< endl;
+    MyFile << 'Camera1.k2: "'<< left_camera_info.distortion_model <<'"'<< endl;
+    MyFile << 'Camera1.k3: "'<< left_camera_info.distortion_model <<'"'<< endl;
 
-    MyFile << 'Camera2.p1: "'<< cam_info.distortion_model <<'"'<< endl;
-    MyFile << 'Camera2.p2: "'<< cam_info.distortion_model <<'"'<< endl;
+    MyFile << 'Camera1.p1: "'<< right_camera_info.distortion_model <<'"'<< endl;
+    MyFile << 'Camera1.p2: "'<< right_camera_info.distortion_model <<'"'<< endl;*/
 
-    MyFile << 'Camera2.p1: "'<< cam_info.distortion_model <<'"'<< endl;
-    MyFile << 'Camera2.p2: "'<< cam_info.distortion_model <<'"'<< endl;
+    MyFile << "Camera2.fx: " << right_camera_info.k[0] << endl;
+    MyFile << "Camera2.fy: "<< right_camera_info.k[4] << endl;
 
-    MyFile.close();*/
+    MyFile << "Camera2.cx: "<< right_camera_info.k[2] << endl;
+    MyFile << "Camera2.cy: "<< right_camera_info.k[5]  << endl;
+
+    MyFile.close();
 }
